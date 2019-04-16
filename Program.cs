e9784a14401e2d5a42f12f5a86b635907e964f62
User <User@DEV-022783.OnBase.net>
2019-04-16 17:57:54 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace htc_final_project_Arya_Singh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
